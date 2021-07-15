d6de9bafdc01b3e49f87847b9ffa4e566002a34e
Jordan Grace <jordan.t.grace@gmail.com>
2021-07-15 09:32:59 -0400

    Working on charts

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,8 +1,27 @@
 <script>
+    import GammaExposure from './GammaExposure.svelte'
+
+    let symbol = null;
+    let data = null;
+
+    let el;
+
+    async function handleSubmit() {
+        data = await (await fetch(`http://localhost:3030/gamma/${symbol}`)).json();
+        console.log(data);
+	}
 </script>
 
 <main>
-    Hi
+    Symbol:
+    <input bind:value={symbol}>
+    <button on:click={handleSubmit}>
+        Submit
+    </button>
+
+    {#if data}
+        <GammaExposure data={data}/>
+    {/if}
 </main>
 
 <style>
---
+++ frontend/src/GammaExposure.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	
+    export let data;
+	
+	let el;
+
+	onMount(() => {
+        let d = [];
+        for (const [key, value] of Object.entries(data)) {
+            d.push(value);
+        }
+		d3.select(el)
+			.selectAll("div")
+			.data(d)
+			.enter()
+			.append("div")
+			.style("width", function(d) {
+				return d + "px";
+			})
+			.text(function(d) {
+				return d;
+			});
+    });
+</script>
+
+<main>
+    <div bind:this={el} class="chart"></div>
+</main>
+
+<style>
+    .chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
+</style>
