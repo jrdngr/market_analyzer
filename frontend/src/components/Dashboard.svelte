4fd0277501950c5c2536684ba275d235f55defab
Jordan Grace <jordan.t.grace@gmail.com>
2021-08-21 21:15:08 -0400

    Fixed tickers and added search link

--- frontend/src/components/Dashboard.svelte
+++ frontend/src/components/Dashboard.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { Link } from "svelte-routing";
     import GammaMap from './GammaMap.svelte';
 </script>
 
@@ -8,17 +9,18 @@
             <GammaMap symbol="SPY"/>
         </div>
         <div class="map">
-            <GammaMap symbol="SPY"/>
+            <GammaMap symbol="SPX"/>
         </div>
     </div>
     <div class="row">
         <div class="map">
-            <GammaMap symbol="SPX"/>
+            <GammaMap symbol="QQQ"/>
         </div>
         <div class="map">
             <GammaMap symbol="NDX"/>
         </div>
     </div>
+    <Link to="/search">Search</Link>
 </main>
 
 <style>
