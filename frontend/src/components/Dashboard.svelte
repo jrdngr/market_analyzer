131e137966bb46a61fca309ff2bff3248f65170a
Jordan Grace <jordan.t.grace@gmail.com>
2021-09-04 15:25:49 -0400

    Added menu on right

--- frontend/src/components/Dashboard.svelte
+++ frontend/src/components/Dashboard.svelte
@@ -5,6 +5,7 @@
     const DASHBOARD_ROWS_KEY = "dashboardRows";
 
     let width = 80;
+    let menuHidden = false;
 
     let chartRows = loadRows();
     saveRows();
@@ -43,25 +44,44 @@
 </script>
 
 <main style="--main-width: {width}%">
-    {#each chartRows as row}
-        <div class="row">
-            {#each row as options}
-                <div class="map">
-                    <GammaMap options={options}/>
-                </div>
-            {/each}
+    <div class="charts">
+        {#each chartRows as row}
+            <div class="row">
+                {#each row as options}
+                    <div class="map">
+                        <GammaMap options={options}/>
+                    </div>
+                {/each}
+            </div>
+        {/each}
+        <div class="chart-controls">
+            <input type="range" min="10" max="100" step=1 bind:value={width}>
         </div>
-    {/each}
-
-    <div class="controls">
-        <input type="range" min="10" max="100" step=1 bind:value={width}>
     </div>
-    <Link to="/search">Search</Link>
+
+    <div class="menu">
+        <button on:click={() => menuHidden = !menuHidden}>{menuHidden ? "+" : "-" }</button>
+        {#if !menuHidden}
+            <div class="links">
+                <Link to="/search">Search</Link>
+            </div>
+        {/if}
+    </div>
 </main>
 
 <style>
     main {
         width: var(--main-width);
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .charts {
+        width: 100%;
+    }
+
+    .menu {
+        width: 1%;
     }
 
     .row {
@@ -73,7 +93,7 @@
         width: 100%;
     }
 
-    .controls input {
+    .chart-controls input {
         width: 100%;
     }
 </style>
